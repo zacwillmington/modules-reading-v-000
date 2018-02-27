b32ac1c05c906a7fa231f2f05ff3932c2ff59d3d
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 03:10:47 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -4,4 +4,12 @@ module Dance
         "I'm twirling!"
     end
 
+    def pirouette
+        "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+
 end
