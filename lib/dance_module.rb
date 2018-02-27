5e8d3fff7334e9e90f22410a1a74ee603a513938
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 03:45:07 +0000

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,20 @@
+module Dance
+
+    def twirl
+        "I'm twirling!"
+    end
+
+    def pirouette
+        "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+
+    def jump
+        "Look how high I'm jumping!"
+    end
+
+
+end
