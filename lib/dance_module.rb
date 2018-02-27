519109e407c5528ae2edb98f30074d642ff3c0c0
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 03:36:42 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,25 +1,16 @@
-# module FancyDance
-#
-#     module instanceMethods
-#
-#         def twirl
-#             "I'm twirling!"
-#         end
-#
-#         def pirouette
-#             "I'm doing a pirouette"
-#         end
-#
-#         def take_a_bow
-#             "Thank you, thank you. It was a pleasure to dance for you all."
-#         end
-#     end
-#
-#     module ClassMethods
-#
-#         def metadata
-#             "This class produces objects that love to dance."
-#         end
-#     end
-#
-# end
+module Dance
+
+    def twirl
+        "I'm twirling!"
+    end
+
+    def pirouette
+        "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+    
+
+end
