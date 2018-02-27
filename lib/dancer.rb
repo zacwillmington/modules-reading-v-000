b48bfd9a30b40e952a994bb0495e80de74eb0c46
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 03:13:53 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,5 +2,12 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+    include Dance
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
 
 end
